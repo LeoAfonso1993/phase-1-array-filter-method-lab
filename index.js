0689677874c5f4902f9d82719e25eaa529e8d7b7
Leandro Afonso <leandro.afonso@hotmail.com>
2022-03-31 20:06:04 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, name){
+    function checkName(i){
+        return i.toUpperCase() == name.toUpperCase()
+    }
+    return array.filter(checkName) 
+    /*return array.filter(function(i){
+        if(i == name){
+            return i
+    }})*/
+}
+
+console.log(findMatching(drivers, "Sammy"))
+
+
+function fuzzyMatch(array,name) {
+    function checkFirstLetter(element) {
+        return element[0].toUpperCase() == name[0].toUpperCase()
+    }
+    return array.filter(checkFirstLetter)
+}
+
+console.log(fuzzyMatch(drivers, "Sammy"))
+
+
+const drivers1 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function matchName(array, name){
+    return array.filter((element) => element.name === name)
+}
+console.log(matchName(drivers1, "Bobby"))
